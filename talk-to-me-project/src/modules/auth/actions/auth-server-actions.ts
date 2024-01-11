f04d17229d7ef4262af48e25a589e1014a71854e
Bruna Varela <brunavarela1@hotmail.com>
2024-01-11 09:46:26 -0300

    chore: create invalid email/password feedback

---
+++ talk-to-me-project/src/modules/auth/actions/auth-server-actions.ts
@@ -0,0 +1,111 @@
+"use server";
+
+import { PrismaClient } from "@prisma/client";
+import * as bcrypt from "bcrypt";
+import { redirect } from "next/navigation";
+import AuthService from "../services/auth-service";
+
+const prisma = new PrismaClient();
+
+export async function createAccount(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashPassword,
+    },
+  });
+
+  redirect("/pages/sign-in");
+}
+
+// async function login(formData: FormData) {
+//   "use server";
+
+//   const email = formData.get("email") as string;
+//   const password = formData.get("password") as string;
+
+//   const user = await prisma.user.findFirst({
+//     where: {
+//       email,
+//     },
+//   });
+
+//   // se o usuário não existir
+//   if (!user) {
+//     //aqui posso usar optmistic update para atualizar a tela
+//     console.log("Error");
+//     redirect("/pages/sign-in");
+//   }
+
+//   const isMatch = await bcrypt.compare(password, user?.password);
+
+//   // se email e senha não forem compatíveis
+//   if (!isMatch) {
+//     console.log("Usuário ou senha inválidos");
+//     redirect("/pages/sign-in");
+//   }
+
+//   // Se o usuário e a senha forem válidos
+//   // criar aqui a sessão com JWT
+//   await AuthService.createSessionToken({
+//     sub: user.id,
+//     name: user.name,
+//     email: user.email,
+//   });
+
+//   redirect("/pages/dashboard");
+// }
+
+export async function login(formData: FormData) {
+  "use server";
+
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  // se o usuário não existir
+  if (!user) {
+    // retornar false indicando falha no login
+    return false;
+  }
+
+  const isMatch = await bcrypt.compare(password, user?.password);
+
+  // se email e senha não forem compatíveis
+  if (!isMatch) {
+    console.log("Usuário ou senha inválidos");
+    return false;
+  }
+
+  // Se o usuário e a senha forem válidos
+  // criar aqui a sessão com JWT
+  await AuthService.createSessionToken({
+    sub: user.id,
+    name: user.name,
+    email: user.email,
+  });
+  redirect("/pages/dashboard");
+  // retornar true indicando login bem-sucedido
+  return true;
+}
+
+// const AuthActions = {
+//   createAccount,
+//   login,
+// };
+
+// export default AuthActions;
--- talk-to-me-project/src/modules/auth/components/signin-form.tsx
+++ talk-to-me-project/src/modules/auth/components/signin-form.tsx
@@ -1,26 +1,37 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+"use client";
+
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import AuthActions from "../actions/auth-actions";
+import { useState, FormEvent } from "react";
 
 export default function SignInForm() {
+  const [invalidEmail, setInvalidEmail] = useState(false);
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const success = await AuthActions.login(formData);
+
+    if (!success) {
+      setInvalidEmail(true);
+    } else {
+      setInvalidEmail(false);
+    }
+  };
+
   return (
     <Card>
-      <form action={AuthActions.login}>
+      <form onSubmit={handleSubmit}>
         <CardContent>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
               <Input id="email" name="email" placeholder="Seu email" required />
             </div>
-            <div className="flex flex-col space-y-1.5 ">
+            <div className="flex flex-col space-y-1.5">
               <Input
                 id="password"
                 name="password"
@@ -28,11 +39,18 @@ export default function SignInForm() {
                 type="password"
                 required
               />
+              {invalidEmail && (
+                <span className="text-red-500 text-center">
+                  Usuário ou senha inválidos
+                </span>
+              )}
             </div>
           </div>
         </CardContent>
         <CardFooter className="flex flex-col">
-          <Button type="submit" variant={"secondary"}>Entrar</Button>
+          <Button type="submit" variant={"secondary"}>
+            Entrar
+          </Button>
           <div className="text-primary text-sm mt-4 w-64 text-center">
             <span>
               Ainda não possui conta?
